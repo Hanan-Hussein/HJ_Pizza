558e7f4891bd0620b90cfad417c0b9bbb965553b
Hanan-Hussein <hanhuss035@gmail.com>
2022-03-20 19:48:44 +0300

    ft:fetchingFormValues

--- js/script.js
+++ js/script.js
@@ -1,80 +1,77 @@
-$('#customize').click(function(e){
+$('#customize').click(function(e) {
     e.preventDefault();
-    $('.toppingsAndCrust,#addtoCart,#customize').toggle("slow");
+    $('.toppingsAndCrust,#addtoCart,#customize').toggle("swing");
 
 });
 //pizza constructor
-function Pizza(name,size){
-    this.name=name;
-    this.size=size;
-    this.crustType=[];
-    this.toppings=[];
+function Pizza(name, size) {
+    this.name = name;
+    this.size = size;
+    this.crustType = [];
+    this.toppings = [];
 }
 //fullprice protype
-Pizza.prototype.fullPrice=function(){
-    var totalToppingPrice=0;
-    this.toppings.forEach(topping => {
-        totalToppingPrice+=topping.toppingPrice();
-    });
+Pizza.prototype.fullPrice = function() {
+        var totalToppingPrice = 0;
+        this.toppings.forEach(topping => {
+            totalToppingPrice += topping.toppingPrice();
+        });
 
-    var total=totalToppingPrice+this.sizePrice()+this.crustType[0].crustPrice();
-    return total;
+        var total = totalToppingPrice + this.sizePrice() + this.crustType[0].crustPrice();
+        return total;
 
-}
-//size price prototype
-Pizza.prototype.sizePrice=function(){
-    if(this.size==="small"){
-        return 500;
     }
-    else if(this.size==="medium"){
-        return 800;
+    //size price prototype
+Pizza.prototype.sizePrice = function() {
+        if (this.size === "small") {
+            return 500;
+        } else if (this.size === "medium") {
+            return 800;
+        } else if (this.size === "large") {
+            return 1200;
+        }
     }
-    else if(this.size==="large"){
-        return 1200;
-    }
+    //crustType constructor
+function CrustType(name) {
+    this.name = name;
 }
-//crustType constructor
-function CrustType(name){
-    this.name=name;
-}
-CrustType.prototype.crustPrice=function(){
-    if(this.name==='crispy'){
+CrustType.prototype.crustPrice = function() {
+    if (this.name === 'crispy') {
         return 200;
-    }
-    else if(this.name==='stuffed'){
+    } else if (this.name === 'stuffed') {
         return 250;
-    }
-    else if(this.name==='gluttenFree'){
+    } else if (this.name === 'gluttenFree') {
         return 150;
     }
-   
+
 }
-function Toppings(toppingName){
+
+function Toppings(toppingName) {
     this.toppingName = toppingName;
 }
-Toppings.prototype.toppingPrice=function(){
-    if(this.toppingName==="cheese"){
+Toppings.prototype.toppingPrice = function() {
+    if (this.toppingName === "cheese") {
         return 150;
-    }
-    else if(this.toppingName==="pepporoni"){
+    } else if (this.toppingName === "pepporoni") {
         return 120;
-    }
-    else if(this.toppingName==="olive"){
+    } else if (this.toppingName === "olive") {
         return 100;
-    }
-    else {
+    } else {
         return 0;
     }
-    
+
 }
 
-var mmm=new Toppings("cheese");
-var ol=new Toppings("olive");
+$('#formMenu').submit(function(e) {
+    e.preventDefault();
+    var sizeV = this.sizes.value;
+    var crustName = this.crust.value;
+    var toppingName = [];
+    toppingName = $("input:checkbox:checked").map(function(bbb) {
+        return $(this).val();
+    }).get();
 
-var curs=new CrustType("gluttenFree");
-var jj=new Pizza("jjj","medium");
-
-jj.toppings.push(mmm);
-jj.toppings.push(ol);
-jj.crustType.push(curs);
-console.log(jj.fullPrice());
+    console.log(sizeV);
+    console.log(crustName);
+    console.log(toppingName);
+});
