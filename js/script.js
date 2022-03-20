83aaf7d0a258f15d24fc659aee73c622798b8a24
Hanan-Hussein <hanhuss035@gmail.com>
2022-03-20 12:47:03 +0300

    ft:crustType-checks crust type and appends price

--- js/script.js
+++ js/script.js
@@ -2,4 +2,31 @@ $('#customize').click(function(e){
     e.preventDefault();
     $('.toppingsAndCrust,#addtoCart,#customize').toggle("slow");
 
-})
+});
+//pizza constructor
+function Pizza(name,size,crustType,price,toppings){
+    this.name=name;
+    this.size=size;
+    this.crustType=crustType;
+    this.price=price;
+    this.toppings=toppings;
+}
+
+//crustType constructor
+function CrustType(name){
+    this.name=name;
+ 
+
+}
+CrustType.prototype.crustPrice=function(){
+    if(this.name==='crispy'){
+        return 200;
+    }
+    else if(this.name==='stuffed'){
+        return 250;
+    }
+    else if(this.name==='gluttenFree'){
+        return 150;
+    }
+}
+
